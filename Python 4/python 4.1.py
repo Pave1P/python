1c908ccc156e6aa483dce111d1c82006da112910
Pave1P <145470076+Pave1P@users.noreply.github.com>
2024-09-12 20:56:37 +0200

    Delete Python 4 directory

--- Python 4/python 4.1.py
+++
@@ -1,5 +0,0 @@
-s=[int(i) for i in input().split(',')]
-a=set()
-for i in range(len(s)):
-    a.add(s[i])
-print(len(a))
--- Python 4/python 4.2.py
+++
@@ -1,8 +0,0 @@
-s=set(int(i) for i in input().split(','))
-a=set(int(i) for i in input().split(','))
-if s==a:
-    print("False")
-elif s.issubset(a):
-    print('True')
-else:
-    print('False')
