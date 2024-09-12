c8e3228795073c3ddd57fe4b30018c7f585bf4a6
Pave1P <145470076+Pave1P@users.noreply.github.com>
2024-09-12 20:56:05 +0200

    Delete Практическое задание 9 directory

--- Практическое задание 9/4/4.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-x = np.array([6, 2, 0, 3, 0, 0, 5, 7, 0])
-indices = np.where(x[:-1] == 0)[0]
-max_after_zero = np.max(x[indices + 1])
-
-print("Максимальный элемент после нуля:", max_after_zero)
--- Практическое задание 9/8/8.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-array = np.array([0, 1, 2, 0, 0, 4, 0, 6, 9])
-nonzero_indices = np.nonzero(array)
-
-print("Индексы ненулевых элементов:", nonzero_indices)
